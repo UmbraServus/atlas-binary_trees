6288f9e40cdb2dee8999af759b475a9d8e4e5024
kiko sprague <ksprague0592@gmail.com>
2024-04-10 21:32:56 +0000

    recursively free trees

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *current;
+
+	if (tree == NULL);
+	{
+		exit
+	}
+
+		void binary_tree_delete(tree->left);
+		void binary_tree_delete(tree->right);
+		
+		free(tree);
+}
