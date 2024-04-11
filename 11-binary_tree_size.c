47a7cf099c1908047484f08d66d410a4b70198a0
kiko sprague <ksprague0592@gmail.com>
2024-04-11 17:19:02 +0000

    adding func to find size of a tree from whatever node is given using recursion

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size- size of the tree from given node
+ * @tree: given node of a tree to find the size
+ *
+ * Return: number of nodes or size in the tree
+ *
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 1;
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+
+	size = binary_tree_size(tree->left) + binary_tree_size(tree->right);
+
+	return (size + 1);
+}
