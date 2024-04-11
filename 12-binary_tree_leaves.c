a55f42c433a2b9799e5aef38ededd5c19cce292a
kiko sprague <ksprague0592@gmail.com>
2024-04-11 17:59:50 +0000

    12-leaves function to count the number of leaves in a tree by recursively calling on the left and right then checking if tree->left/right are both null and returning L + R + 1 then adding them together.

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves- counts the leaves in a tree from a given node
+ * @tree: the node given to count the leaves of.
+ *
+ *
+ * Return: 0 if tree is NULL or number of leaves. 
+ *
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t left_leaves, right_leaves;
+
+	if (tree == NULL)
+	{
+		return(0);
+	}
+	
+	left_leaves = binary_tree_leaves(tree->left);
+	right_leaves = binary_tree_leaves(tree->right);
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return(left_leaves + right_leaves + 1);
+	}
+
+	return(left_leaves + right_leaves);
+}
