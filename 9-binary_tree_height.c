e062394cf77e5813bd7ce87c52b2970ac8c3e170
kiko sprague <ksprague0592@gmail.com>
2024-04-11 04:41:54 +0000

    forgot to add height onto variable

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -26,7 +26,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (left_height + 1);
 	}
 	
-	else if (right_height > left)
+	else if (right_height > left_height)
 	{
 			return (right_height + 1);
 	}
