efd836657074649eaaeb3b059ca07d7469e573c0
kiko sprague <ksprague0592@gmail.com>
2024-04-11 02:40:01 +0000

    traverse thru the tree from root from left nodes to the right nodes

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder- traverses the binary tree from left nodes to  right nodes
+ * @tree: tree to be traversed
+ * @func used to print numbers of each node we visit
+ *
+ * Return: void
+ *
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+
+	func(tree->n);
+
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
