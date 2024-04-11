c83210fac715763e3f16ddf531dc8054ea526e82
kiko sprague <ksprague0592@gmail.com>
2024-04-11 04:36:22 +0000

    added a to the if statment in 9-binaryheight that returns 0 if root is the only node given

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,14 +13,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height = 0;
 	size_t right_height = 0;
 
-	if (tree == NULL)
+	if (tree == NULL || tree->parent == NULL)
 	{
 		return(0);
 	}
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-
+	
 	if (left_height > right_height)
 	{
 		return (left_height + 1);
