452ece48dc3e8aab4dba76d6576da20690e82522
kiko sprague <ksprague0592@gmail.com>
2024-04-11 20:55:48 +0000

    added the function for height calc into the function for balance factor calc

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,32 +10,50 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left, right;
-	
+	size_t left, right;
+	int balance;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	{
+		return (0);
+	}
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	balance = left - right;
+
+	return (balance);
+}
+
+/**
+ * binary_tree_height- gets the height of the tree
+ * @tree: tree to find the height of
+ *
+ * Return: height of tree or 0 if no tree
+ *
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
-	if (tree->parent == NULL && tree->left == NULL && tree->right == NULL)
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
 	{
-		return (0);
+		return (left_height + 1);
 	}
 
-	if (tree->left == NULL && tree->right == NULL)
+	else
 	{
-		return (1);
+			return (right_height + 1);
 	}
-
-
-	left = binary_tree_balance(tree->left);
-	right = binary_tree_balance(tree->right);
-
-	if (left == right)
-	{
-		return (0);
-	}
-
-	return (left - right);
 }
-
