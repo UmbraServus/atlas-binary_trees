55a8bede058676f5e343e59c1e5190b81b823082
kiko sprague <ksprague0592@gmail.com>
2024-04-11 04:50:17 +0000

    idk

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,7 +13,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height = 0;
 	size_t right_height = 0;
 
-	if (tree == NULL || tree->left == NULL  && tree->right == NULL)
+	if (tree == NULL || (tree->left == NULL  && tree->right == NULL))
 	{
 		return (0);
 	}
