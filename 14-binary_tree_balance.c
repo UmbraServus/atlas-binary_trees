30b16aea1f369015324ea61b23e0f55187f5be41
kiko sprague <ksprague0592@gmail.com>
2024-04-11 19:56:59 +0000

    give balance factor after finding Left subroot height and Right subroot height

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance- gives the balance factor of the tree at given node.
+ * @tree: tree that the given node comes from
+ *
+ * Return: 0 if tree is null or balance factor L height - R height.
+ *
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left, right;
+	
+	if (tree == NULL )
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+
+
+	left = binary_tree_balance(tree->left);
+	right = binary_tree_balance(tree->right);
+
+	return (left - right);
+}
+
