f4988d9341d6cf53f0bea1ab3821b8722cb1264c
kiko sprague <ksprague0592@gmail.com>
2024-04-12 03:03:10 +0000

    trying something new

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -18,21 +18,15 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	}
 
-	L = binary_tree_is_full(tree->left);
-	R = binary_tree_is_full(tree->right);
-
-	if (L ==  0 || R == 0)
-	{
-		return (0);
-	}
-
-	if ((tree->left != NULL && tree->right != NULL) || (tree->left == NULL && tree->right == NULL))
-	{
-		return (0);
-	}
-	
-	else
+	if (tree->left  ==  NULL &&  tree->right == NULL)
 	{
 		return (1);
 	}
+
+	if ((tree->left) && (tree->right))
+	{
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	}
+
+		return (0);
 }
