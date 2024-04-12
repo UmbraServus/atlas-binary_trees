89804790aaf02fab7e0c049c3e33e1eb04c296f2
kiko sprague <ksprague0592@gmail.com>
2024-04-12 02:13:44 +0000

    took out the int comparisons

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,27 +11,25 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int L, R;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
-	L = binary_tree_is_full(tree->left);
-	R = binary_tree_is_full(tree->right);
+	binary_tree_is_full(tree->left);
+	binary_tree_is_full(tree->right);
 
-	if (L == NULL || R == NULL)
+	if (tree->left NULL || tree->right == NULL)
+	{
+		return (0);
+       
+	if (tree->left == NULL && tree->right == NULL)
 	{
 		return (0);
 	}
-       
-	if (tree->left != NULL && tree->right != NULL)
-	{
-		return (1);
-	}
 	
-	if (tree->left == NULL && tree->right == NULL)
+	else  (tree->left == NULL && tree->right == NULL)
 	{
 		return (0);
 	}
