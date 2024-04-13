03cd7fce92487e807eb22bc71c88a0165528ca14
kiko sprague <ksprague0592@gmail.com>
2024-04-13 01:40:39 +0000

    added an if statement to handle single node case

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -25,6 +25,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
 	
 	return (1);
 }
