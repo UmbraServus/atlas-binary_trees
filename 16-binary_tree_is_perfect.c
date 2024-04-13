276068eead854d34ae81a37707eb679b884ad62a
kiko sprague <ksprague0592@gmail.com>
2024-04-13 02:09:46 +0000

    forgot a ;

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -16,7 +16,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (tree->left == NULL || tree->right == NULL)
+	if ((tree->left == NULL && tree->right != NULL) || (tree->left != NULL && tree->right == NULL))
 	{
 		return (0);
 	}
@@ -25,14 +25,14 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (1);
 	}
 	
-	if (tree->left->left == NULL || tree->right->left == NULL)
+	if ((tree->left->left == NULL && tree->right->left != NULL) ||  (tree->left->left != NULL tree->right->left == NULL))
 	{
 		return (0);
 	}
 
-	if (binary_tree_is_perfect(tree->left) || binary_tree_is_perfect(tree->right))
+	if (!binary_tree_is_perfect(tree->left) || !binary_tree_is_perfect(tree->right))
 	{
-		return (0)
+		return (0);
 	}
 
 	return (1);
