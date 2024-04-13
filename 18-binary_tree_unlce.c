24ea74e9ba79f419a022a3198e88e1de7b990b0e
kiko sprague <ksprague0592@gmail.com>
2024-04-13 02:51:51 +0000

    18 - binary tree uncle checks for uncle by runnign the parent thru sibling function

---
+++ 18-binary_tree_unlce.c
@@ -0,0 +1,51 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling- checks for a sibling of the current node
+ * @node: current node
+ *
+ *
+ * Return: NULL if no sibling, current node is NULL or is root
+ *
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+
+	if (node->parent->left == node && node->parent->right != NULL)
+	{
+		return (node->parent->right);
+	}
+
+	if (node->parent->right == node && node->parent->left != NULL)
+	{
+		return (node->parent->left);
+	}
+
+	return (NULL);
+}
+
+/**
+ * binary_tree_unlce- checks if node has an uncle
+ * @node: node to be chcked
+ *
+ *
+ *
+ * Return: NULL or the node that is the uncle
+ *
+ */
+
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+
+	return(binary_tree_sibling(node->parent));
+}
