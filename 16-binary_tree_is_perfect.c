79de218043d179ee73ef006d808162c0e65ee7de
kiko sprague <ksprague0592@gmail.com>
2024-04-13 01:34:26 +0000

    took out a ; that I accidently placed at the end of an if condition

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -16,14 +16,14 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (tree->left == NULL || tree->right == NULL);
+	if (tree->left == NULL || tree->right == NULL)
 	{
 		return (0);
 	}
 
 	if ((tree->left) && (tree->right))
 	{
-		return (binary_tree_is_perfect(tree->left) && binary(tree->right));
+		return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 	}
 	
 	return (1);
